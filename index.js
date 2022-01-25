668b7238148ee6af44943ed9672532ff2f7fd099
vickoritz <vickor@attensi.com>
2022-01-25 21:59:34 +0100

    feat: display the sum of the cards

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 let firstCard = 10
-let secondCard = 11
-let sum = firstCard + secondCard + 4
+let secondCard = 4
+let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
-
 let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
 
 function startGame() {
+    result = firstCard + secondCard
+    sumEl.textContent = "Sum: " + result
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack!"
+        message = "You've got Blackjack!"
         hasBlackJack = true
     } else {
         message = "You're out of the game!"
